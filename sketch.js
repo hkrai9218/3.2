9935d846da0290e24ab8305e30d6dcc9f16c09bb
hkrai9218 <hkrai9218@gmail.com>
2022-05-11 21:19:48 -0700

    Trying to change works

--- sketch.js
+++ sketch.js
@@ -4,10 +4,17 @@ function setup() {
 
 function draw() {
   background(206, 184, 228);
-  
+
   drawCharacter();
-  
+
   drawLines();
+
+  if (keyPressed)
+    triangle(200, 175, 185, 210, 220, 215)
+    line(width * .489, height * 0.53, width * 0.5, height * 0.58);
+    line(width * .513, height * 0.534, width * 0.525, height * 0.58);
+    line(width * .45, height * 0.5, width * 0.475, height * 0.49);
+    line(width * .529, height * 0.49, width * 0.57, height * 0.51);
 }
 
 function drawCharacter(){
@@ -19,5 +26,15 @@ function drawLines(){
   line(width * .513, height * 0.534, width * 0.525, height * 0.58);
   line(width * .45, height * 0.5, width * 0.475, height * 0.49);
   line(width * .529, height * 0.49, width * 0.57, height * 0.51);
+
+}
+
+function keyPressed(){
+
+  if (key === 'd'){
+    console.log(`key is being pressed`)
+  }
+
+  return false;
   
 }
